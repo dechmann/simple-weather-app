d727be8bfb88be5eae0e727af7ef4cee5a631559
dechmann <tdechmann001@my.hennepintech.edu>
2016-04-06 20:05:22 -0500

    finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(function(){
     
-    function Weather(){
-        $.getJSON("//api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&APPID=" + key + "")
+    function weather(zip, key){
+        $.getJSON("//api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=" + key)
         .done(function(data){
-            $("#weather").append("<p>It is " + data.main.temp[2] + " F, in " + data.name + "</p>");
+            $("#weather").append("<p>It is " + data.main.temp + " F, in " + data.name + "</p>");
         }).fail(function(){
-            alert("<p>sorry we are having trouble displaying your data</p>");
+            $("#weather").append("<p>sorry we are having trouble displaying your data</p>");
         });
     }
-/*function weather(zip) {
-    $.ajax({url: "api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us",
+/*function weather(zip, key) {
+    $.ajax({url: "api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&APPID=" + key,
 success: function(data) {
     $("#weather").append("<p>It is " + data.main.temp[2] + " F, in " + data.name + "</p>");
 },
@@ -22,18 +22,18 @@ error: function(data) {
         document.getElementById("zip");
     });
     
-    $("#wether").apped(button)*/
+    $("#wether").apped(button) */
     
     //button info and useage goes here.
     
     $("#submit").on("click", function(event) {
         event.preventDefault();
-   
-    var zip = $("#zip").val();
-    var key = $("#key").val();
 
-    weather(zip);
+        var zip = $("#zip").val();
+        var key = $("#key").val();
+
+    weather(zip, key);
     }); 
-    });
+});
 
 //apikey - 042760549d18d25fa9442d2fc978cee1
